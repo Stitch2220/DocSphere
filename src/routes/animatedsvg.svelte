999254ef85c6c8c6288273b2dbcc7bb9e9c4be97
Afonso <afonso.couteiro@hotmail.com>
2024-07-29 00:43:44 +0100

    test

---
+++ src/routes/animatedsvg.svelte
@@ -0,0 +1,60 @@
+
+<style>
+    .icon {
+        width: 200px;
+        height: 200px;
+        margin: auto;
+        display: block;
+    }
+</style>
+
+<script>
+    import anime from 'animejs';
+    import { onMount } from 'svelte';
+    let body,
+        leftEye,
+        rightEye;
+
+    onMount(setup);
+
+    function setup() {
+        anime.timeline({
+            easing: 'easeInSine',
+            autoplay: true,
+            duration: 1000,
+        }).add({
+            targets: body,
+            scale: [0, 1],
+            easing: 'easeInOutExpo',
+        });
+    }
+</script>
+
+<div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="203" height="181" viewBox="0 0 203 181" fill="none" filter="url(#dropShadow)">
+        <defs>
+            <filter id="dropShadow" x="-20%" y="-20%" width="140%" height="140%">
+                <feDropShadow dx="0" dy="0" stdDeviation="8" flood-color="black" flood-opacity="0.5"/>
+            </filter>
+        </defs>
+        <circle bind:this={body} cx="101.062" cy="67.0625" r="59.0625" fill="url(#paint0_diamond_59_76)"/>
+        <ellipse bind:this={leftEye} cx="78.3498" cy="57.8113" rx="11.3876" ry="17.5063" transform="rotate(10 78.3498 57.8113)" fill="white"/>
+        <ellipse bind:this={rightEye} cx="11.3876" cy="17.5063" rx="11.3876" ry="17.5063" transform="matrix(0.984808 -0.173648 -0.173648 -0.984808 115.726 77.0291)" fill="white"/>
+        <g filter="url(#filter0_f_59_76)">
+            <path d="M117.232 91.8526C105.3 100.927 93.6956 97.5044 85.499 91.7024C82.0406 89.2544 84.1186 84.4839 88.3558 84.4839H114.19C118.399 84.4839 120.583 89.3048 117.232 91.8526Z" fill="white" fill-opacity="0.2"/>
+        </g>
+        <defs>
+            <filter id="filter0_f_59_76" x="0.257507" y="0.983887" width="202.111" height="179.951" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="41.75" result="effect1_foregroundBlur_59_76"/>
+            </filter>
+            <radialGradient id="paint0_diamond_59_76" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(11.8314 14.2037) rotate(2.02892) scale(242.436)">
+                <stop stop-color="#EDE0D4"/>
+                <stop offset="0.37413" stop-color="#DDB892"/>
+                <stop offset="0.8" stop-color="#4895EF"/>
+            </radialGradient>
+        </defs>
+    </svg>
+
+</div>
---
+++ src/routes/background.svelte
@@ -0,0 +1,16 @@
+<script>
+    // You can add any JavaScript logic if needed
+</script>
+
+<style global>
+    @import 'tailwindcss/tailwind.css';
+
+    .gradient-background {
+        background: linear-gradient(270deg, #ff9a9e, #fad0c4, #fad0c4, #ff9a9e);
+        background-size: 400% 400%;
+    }
+</style>
+
+<div class="w-full h-screen gradient-background animate-gradient">
+    <!-- Content goes here -->
+</div>
